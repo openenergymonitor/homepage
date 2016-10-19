5ec5df505b5f9d971693aac4ffbee4718501ee77
Glyn Hudson <glyn.hudson@gmail.com>
2016-10-19 17:22:43 +0100

    front controller p2

--- index.php
+++ index.php
@@ -18,32 +18,77 @@ ini_set('display_errors', 'on');
 date_default_timezone_set('Europe/London');
 
 require "core.php";
+$path = get_application_path();
 
-$q = "home";
+$q = "home.html";
 if (isset($_GET['q'])) $q = $_GET['q'];
 
 $format = "html";
+$themed = true;
 $content = "Sorry page not found";
 
-$format = "html";
-$content = view("pages/$q.html",array());
+if ($q=="home.html") $themed = false;
 
-if ($q!="home") {
-$content = view("theme/theme.php",array("content"=>$content));
+
+
+$filename_parts = explode(".",$q);
+if (count($filename_parts)==2)
+    $doc_ext = $filename_parts[count($filename_parts)-1];
+if (count($filename_parts)==1) {
+    $doc_ext = "html";
+    $q .= ".".$doc_ext;
+}
+    
+if (file_exists("pages/".$q)) {
+
+    $content = file_get_contents("pages/".$q);
+    
+    // Parse markdown if page is markdown
+    if ($doc_ext=="md") {
+        include "lib/Parsedown.php";
+        $Parsedown = new Parsedown();
+        $content = $Parsedown->text($content);
+    }
+    
+    if ($doc_ext=="png") $format = "png";
+    if ($doc_ext=="jpg") $format = "jpg";
+    if ($doc_ext=="js") $format = "js";
+    if ($doc_ext=="zip") $format = "zip";
 }
 
 switch ($format) 
 {
     case "html":
         header('Content-Type: text/html');
-        print $content;
+        if ($themed) {
+            print view("theme/theme.php", array("content"=>$content));
+        } else {
+            print $content;
+        }
         break;
     case "text":
         header('Content-Type: text/plain');
+        print $content;
+        break;
+    case "zip":
+        header("Content-Type: application/zip");
+        print $content;
+        break;
+    case "js":
+        header('Content-Type: application/javascript');
         print $content;
         break;
     case "json":
         header('Content-Type: application/json');
         print json_encode($content);
         break;
+    case "png":
+        header('Content-Type: image/png');
+        print $content;
+        break;
+    case "jpg":
+        header('Content-Type: image/jpeg');
+        print $content;
+        break;
+        
 }
--- theme/theme.php
+++ theme/theme.php
@@ -1,9 +1,9 @@
-
+<?php global $path; ?>
 
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <link href="https://fonts.googleapis.com/css?family=Ubuntu:300" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $path; ?>theme/style.css" />
 
 <body>
 
