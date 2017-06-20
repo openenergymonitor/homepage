360b0ff7c60049ce07fbe2452ba2ade8e419c36d
slyremarks <gwil.noble@openenergymonitor.org>
2017-06-20 14:13:18 +0100

    new search tool

---
+++ pages/search.php
@@ -0,0 +1,34 @@
+<?php global $path; ?>
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#44b3e2" />
+
+<title>Home | OpenEnergyMonitor</title>
+<link rel="shortcut icon" href="<?php echo $path; ?>theme/favicon.ico" />
+<!-- Load font locally to enable full offline use, un comment below to load font remotely-->
+<!--<link href="https://fonts.googleapis.com/css?family=Ubuntu:300" rel="stylesheet" type="text/css">-->
+<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
+<link rel="stylesheet" type="text/css" href="<?php echo $path; ?>fonts/ubuntu.css?family=Ubuntu:300">
+<link rel="stylesheet" type="text/css" href="<?php echo $path; ?>theme/font-awesome.min.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $path; ?>theme/style.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<body>
+  <div>
+    
+    TEST
+    <script>
+      (function() {
+        var cx = '006198118389747886812:_nmxikw563w';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+      })();
+    </script>
+    <gcse:searchresults-only></gcse:searchresults-only>
+  </div>
+</body>
