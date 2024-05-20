deeaa204b51227790a5d2268a851fd2602f74838
TrystanLea <trystan.lea@gmail.com>
2024-05-20 10:51:45 +0100

    update about

--- pages/about.php
+++ pages/about.php
@@ -8,9 +8,10 @@
     </p>
     <p>By using energy monitoring, modelling and assessment tools we can take an informed approach to working out what are the best measures to apply. We can then check to ensure solutions meet their expected performance and continue to work well over time.
     </p>
-    <p>To explore the OpenEnergyMonitor energy monitoring system see the <a href="https://guide.openenergymonitor.org" style="color:#44b3e2;">Guide</a>.
+    <p>To explore the OpenEnergyMonitor energy monitoring system see <a href="https://docs.openenergymonitor.org" style="color:#44b3e2;">Docs</a>.
     </p>
-    <p>To learn more about our research into zero carbon energy systems see the sustainable energy section of <a href="https://learn.openenergymonitor.org" style="color:#44b3e2;">Learn</a>.
+    <p>To learn more about our research into zero carbon energy systems see the sustainable energy section of <a href="https://docs.openenergymonitor.org/sustainable-energy/introduction.html" style="color:#44b3e2;">Learn</a></p>
+    <p>Our current research focus is on understanding the performance of heat pumps, see <a href="https://heatpumpmonitor.org" style="color:#44b3e2;">HeatpumpMonitor.org</a>.
     </p>
     <p>All of our work is open source (hardware & software). We believe open source is a better way of doing things where we can learn from and build upon each others work, creating better solutions to the challenges we face.
     </p>
@@ -20,7 +21,7 @@
     <img src="<?php echo $path; ?>pages/img/megniteam.jpg" style="width:100%">
     <p><strong>Project founders and maintainers:</strong> Trystan Lea and Glyn Hudson.
     </p>
-    <p><strong>We would like to give a special thank you to community forum moderators:</strong> Robert Wall, Brian Orpin, Paul Burnell, Bill Thomson, Jon Murphy, Paul Reed, Eric Wouters (bidouilleur) – for all of their hard work supporting the project and helping out on the <a href="https://community.openenergymonitor.org" style="color:#44b3e2;">forums</a>.
+    <p><strong>We would like to give a special thank you to community forum moderators:</strong> Robert Wall, Brian Orpin, Bill Thomson, Sarah Hanson, Tim Bones and Mick Wall - for all of their hard work supporting the project and helping out on the <a href="https://community.openenergymonitor.org" style="color:#44b3e2;">forums</a>.
     </p>
     <h2>Eryri/Snowdonia</h2>
     <p>The OpenEnergyMonitor shop is based in Eryri (Snowdonia) North Wales. We are keen climbers, walkers and mountain bikers.
