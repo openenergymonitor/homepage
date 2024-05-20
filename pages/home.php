0b64d86cc4e4211d7205512db7d85da221609b82
TrystanLea <trystan.lea@gmail.com>
2024-05-20 10:35:30 +0100

    cleanup

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,6 @@
 <?php global $path; ?>
 
+<!-- Top banner -->
 <div style="background-color:#fff">
     <div class="cover" style="height:200px;background-image: url('<?php echo $path; ?>pages/img/gwynt.jpg');">
         <div class="oemName">
@@ -15,103 +16,99 @@
     </div>
     <div style="clear:both"></div>
 </div>
+
+<!-- System description -->
 <div style="background-color:whitesmoke">
     <div class="container">
-        <div>
-            <div class="row">
-                <div class="inner">
-                    <p><span style="font-size:18px">Open source monitoring for home electricity, solar, storage, heat pumps and electric vehicle charging.</span></p>
-                    <p>A versatile and expandable system of sensors and integrations built on the Raspberry Pi and Arduino platforms.</p>
+        <div class="row">
+            <div class="inner">
+                <p><span style="font-size:18px">Open source monitoring for home electricity, solar, storage, heat pumps and electric vehicle charging.</span></p>
+                <p>A versatile and expandable system of sensors and integrations built on the Raspberry Pi and Arduino platforms.</p>
 
-                </div>
             </div>
         </div>
     </div>
 </div>
-<div>
-    <div class="container">
-        <div>
-            <div class="row">
-                <div class="inner">
-                    <br>
-                    <h2 style="color:#000;margin-top:14px;">Introducing the emonPi2</h2>
-                    <p>A 6 channel* electricity monitor with an integrated Raspberry Pi.
-                        Supports single or three-phase monitoring, wired temperature sensing & pulse counting. Can be used as a base-station for further sensor nodes.
-                        Full local or remote data logging and visualisation capability via EmonCMS. <!--Integrates with HomeAssistant and uses MQTT.--></p>
+
+<!-- Main content -->
+<div class="container">
+    <div class="row">
+        <div class="inner">
+            <br>
+            <h2 style="color:#000;margin-top:14px;">Introducing the emonPi2</h2>
+            <p>A 6 channel* electricity monitor with an integrated Raspberry Pi.
+                Supports single or three-phase monitoring, wired temperature sensing & pulse counting. Can be used as a base-station for further sensor nodes.
+                Full local or remote data logging and visualisation capability via EmonCMS. <!--Integrates with HomeAssistant and uses MQTT.--></p>
 
 
-                    <a href="https://shop.openenergymonitor.com/energy-monitoring/"> <img src="<?php echo $path; ?>pages/img/emonPi2_complete_kit__77966.jpg" style="width:100%"></a>
-                    <br>
-                    <h4 style="color:#000;margin-top:14px;">Expand with</h4>
+            <a href="https://shop.openenergymonitor.com/energy-monitoring/"> <img src="<?php echo $path; ?>pages/img/emonPi2_complete_kit__77966.jpg" style="width:100%"></a>
+            <br>
+            <h4 style="color:#000;margin-top:14px;">Expand with</h4>
 
-                    <div style="text-align:left">
-                        <p>
-                            <a href="https://shop.openenergymonitor.com/emontx5-emonpi2-6-ct-expansion-board/"><img src="<?php echo $path; ?>pages/img/ctsensor.png" style="float:left; width:50px; margin-right:20px" /></a>
-                            <b>*Expand to 12 CT channels:</b> Add a further 6 CT channels to the emonPi2 (Pi Zero 2W option only) or emonTx5 to monitor up to 12 circuits with an optional expansion board.
-                        </p><br>
-                        <p>
-                            <a href="https://docs.openenergymonitor.org/emontx5/overview.html"><img src="<?php echo $path; ?>pages/img/emontx5-min.png" style="float:left; width:50px; margin-right:20px" /></a>
-                            <b>emonTx5 (available soon):</b> A 6 channel* electricity monitoring node. Transmits data via 433MHz radio to an emonPi or emonBase. Great for expanding an emonPi2 monitoring setup where more CT inputs are required either in the same location or in a different part of the building.
-                        </p><br>
-                        <p>
-                            <a href="https://shop.openenergymonitor.com/emonth-temperature-humidity-node/"><img src="<?php echo $path; ?>pages/img/emonth-min.png" style="float:left; width:50px; margin-right:20px" /></a>
-                            <b>emonTH2:</b> A battery powered, wireless, room based temperature & humidity monitoring node. Transmits data via 433MHz radio to an emonBase or emonPi.
-                        </p><br>
-                        <p>
-                            <a href="https://shop.openenergymonitor.com/heat-pump-monitoring/"><img src="<?php echo $path; ?>pages/img/heatmeter-min.png" style="float:left; width:50px; margin-right:20px" /></a>
-                            <b>Heat metering:</b> Plug in a M-BUS reader and Modbus reader to the emonPi2 USB ports to read from heat meters and other electricity meters for heat pump monitoring. See also pre-provisioned level 3 heat pump monitor bundle and the <a href="https://heatpumpmonitor.org/" style="color:#2379d6">heatpumpmonitor.org</a> initiative.
-                        </p><br>
-                        <p>
-                            <a href="https://shop.openenergymonitor.com/emonbase-433mhz-rfm69spi/"><img src="<?php echo $path; ?>pages/img/emonbase-min.png" style="float:left; width:50px; margin-right:20px" /></a>
-                            <b>emonBase:</b> A Raspberry Pi base-station that receives data sent from emonTx and emonTH radio nodes. Can be used instead of an emonPi2 where the installation favours locating these in different parts of a building.
-                        </p><br>
-                        <!--<p>
-							<a href="https://shop.openenergymonitor.com/emonbase-433mhz-rfm69spi/"><img src="<?php echo $path; ?>pages/img/emonbase-min.png" style="float:left; width:50px; margin-right:20px" /></a>
-							<b>emonBase:</b> A Raspberry Pi base-station that receives data sent from emonTx and emonTH radio nodes. Can be used instead of an emonPi2 where the installation favours locating these in different parts of a building.
-						</p><br>-->
-                    </div>
-
-                    <h4 style="color:#000;margin-top:14px;">Data logging and visualisation with EmonCMS</h4>
-                    <p>Local on the emonPi2/emonBase. Remote on <a href="https://emoncms.org" style="color:#2379d6">EmonCMS.org</a></p>
-                    <p>Pre built dashboards, advanced graphing tools, flexible input processing.</p>
-                    <a href="https://docs.openenergymonitor.org/emoncms/apps.html"> <img src="<?php echo $path; ?>pages/img/myheatpump.png" style="width:100%"></a>
-
-
-                </div>
+            <div style="text-align:left">
+                <p>
+                    <a href="https://shop.openenergymonitor.com/emontx5-emonpi2-6-ct-expansion-board/"><img src="<?php echo $path; ?>pages/img/ctsensor.png" style="float:left; width:50px; margin-right:20px" /></a>
+                    <b>*Expand to 12 CT channels:</b> Add a further 6 CT channels to the emonPi2 (Pi Zero 2W option only) or emonTx5 to monitor up to 12 circuits with an optional expansion board.
+                </p><br>
+                <p>
+                    <a href="https://docs.openenergymonitor.org/emontx5/overview.html"><img src="<?php echo $path; ?>pages/img/emontx5-min.png" style="float:left; width:50px; margin-right:20px" /></a>
+                    <b>emonTx5 (available soon):</b> A 6 channel* electricity monitoring node. Transmits data via 433MHz radio to an emonPi or emonBase. Great for expanding an emonPi2 monitoring setup where more CT inputs are required either in the same location or in a different part of the building.
+                </p><br>
+                <p>
+                    <a href="https://shop.openenergymonitor.com/emonth-temperature-humidity-node/"><img src="<?php echo $path; ?>pages/img/emonth-min.png" style="float:left; width:50px; margin-right:20px" /></a>
+                    <b>emonTH2:</b> A battery powered, wireless, room based temperature & humidity monitoring node. Transmits data via 433MHz radio to an emonBase or emonPi.
+                </p><br>
+                <p>
+                    <a href="https://shop.openenergymonitor.com/heat-pump-monitoring/"><img src="<?php echo $path; ?>pages/img/heatmeter-min.png" style="float:left; width:50px; margin-right:20px" /></a>
+                    <b>Heat metering:</b> Plug in a M-BUS reader and Modbus reader to the emonPi2 USB ports to read from heat meters and other electricity meters for heat pump monitoring. See also pre-provisioned level 3 heat pump monitor bundle and the <a href="https://heatpumpmonitor.org/" style="color:#2379d6">heatpumpmonitor.org</a> initiative.
+                </p><br>
+                <p>
+                    <a href="https://shop.openenergymonitor.com/emonbase-433mhz-rfm69spi/"><img src="<?php echo $path; ?>pages/img/emonbase-min.png" style="float:left; width:50px; margin-right:20px" /></a>
+                    <b>emonBase:</b> A Raspberry Pi base-station that receives data sent from emonTx and emonTH radio nodes. Can be used instead of an emonPi2 where the installation favours locating these in different parts of a building.
+                </p><br>
+                <!--<p>
+                    <a href="https://shop.openenergymonitor.com/emonbase-433mhz-rfm69spi/"><img src="<?php echo $path; ?>pages/img/emonbase-min.png" style="float:left; width:50px; margin-right:20px" /></a>
+                    <b>emonBase:</b> A Raspberry Pi base-station that receives data sent from emonTx and emonTH radio nodes. Can be used instead of an emonPi2 where the installation favours locating these in different parts of a building.
+                </p><br>-->
             </div>
+
+            <h4 style="color:#000;margin-top:14px;">Data logging and visualisation with EmonCMS</h4>
+            <p>Local on the emonPi2/emonBase. Remote on <a href="https://emoncms.org" style="color:#2379d6">EmonCMS.org</a></p>
+            <p>Pre built dashboards, advanced graphing tools, flexible input processing.</p>
+            <a href="https://docs.openenergymonitor.org/emoncms/apps.html"> <img src="<?php echo $path; ?>pages/img/myheatpump.png" style="width:100%"></a>
+
+
         </div>
     </div>
 </div>
+
+<!-- Shop and documentation links -->
 <div style="background-color:whitesmoke">
     <div class="container">
-        <div>
-            <div class="row">
-                <div class="inner">
-                    <div class="buttonLink">
-                        <div class="guideShop">
-                            <p>All products are available to buy from the shop.</p>
+        <div class="row">
+            <div class="inner">
+                <div class="buttonLink">
+                    <div class="guideShop">
+                        <p>All products are available to buy from the shop.</p>
 
-                            <a href="https://shop.openenergymonitor.com">
-                                <div class="guideShop-buttons">
-                                    Shop
-                                    <i aria-hidden="true" class="fa fa-shopping-cart"></i>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="guideShop">
-                            <p>Get more information on how to set up an OpenEnergyMonitor system.</p>
-                            <a href="https://docs.openenergymonitor.org/overview.html">
-                                <div class="guideShop-buttons">
-                                    Docs
-                                    <i aria-hidden="true" class="fa fa-book"></i>
-                                </div>
-                            </a>
-                        </div>
+                        <a href="https://shop.openenergymonitor.com">
+                            <div class="guideShop-buttons">
+                                Shop
+                                <i aria-hidden="true" class="fa fa-shopping-cart"></i>
+                            </div>
+                        </a>
                     </div>
-                    <div style="clear:both"></div>
+                    <div class="guideShop">
+                        <p>Get more information on how to set up an OpenEnergyMonitor system.</p>
+                        <a href="https://docs.openenergymonitor.org/overview.html">
+                            <div class="guideShop-buttons">
+                                Docs
+                                <i aria-hidden="true" class="fa fa-book"></i>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-        <div style="margin:14px;"></div>
     </div>
 </div>
