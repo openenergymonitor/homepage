a99d6439df8e741dbd08bafca36025ca56dd5583
gwil <gwil.noble@openenergymonitor.org>
2026-06-02 13:25:52 +0100

    redesign

--- pages/about.php
+++ pages/about.php
@@ -16,9 +16,6 @@
     <p>All of our work is open source (hardware & software). We believe open source is a better way of doing things where we can learn from and build upon each others work, creating better solutions to the challenges we face.
     </p>
     <h2>People</h2>
-    <p>The Megni / OpenEnergyMonitor shop team (left to right): Gwil, Glyn, Trystan, Kate, Claire and Amy.
-    </p>
-    <img src="<?php echo $path; ?>pages/img/megniteam.jpg" style="width:100%">
     <p><strong>Project founders:</strong> Trystan Lea and Glyn Hudson
     </p>
     <p>We would like to give a special thank you to community forum moderators: Robert Wall, Brian Orpin, Bill Thomson, Sarah Hanson, Tim Bones and Mick Wall - for all of their hard work supporting the project and helping out on the <a href="https://community.openenergymonitor.org" style="color:#44b3e2;">forums</a>
--- pages/home.php
+++ pages/home.php
@@ -22,7 +22,7 @@
     <div class="container">
         <div class="row">
             <div class="inner">
-                <p><span style="font-size:18px">Open source monitoring for electricity, solar, storage, heat pumps and electric vehicle charging.</span></p>
+                <p><span style="font-size:18px">Open source monitoring for electricity, solar, storage and heat pumps.</span></p>
                 <p>A versatile and expandable system of sensors and integrations built on the Raspberry Pi and Arduino platforms.</p>
 
             </div>
@@ -35,28 +35,24 @@
     <div class="row">
         <div class="inner">
             <br>
-            <h2 style="color:#000;margin-top:14px;">Introducing the emonPi2</h2>
+            <h2 style="color:#000;margin-top:14px;">Introducing the emonPi3</h2>
             <p>A 6 channel electricity monitor with an integrated Raspberry Pi.
                 Supports single or three-phase monitoring, wired temperature sensing & pulse counting. Can be used as a base-station for further sensor nodes.
                 Full local or remote data logging and visualisation capability via EmonCMS. <!--Integrates with HomeAssistant and uses MQTT.--></p>
 
 
-            <a href="https://shop.openenergymonitor.com/energy-monitoring/"> <img src="<?php echo $path; ?>pages/img/emonPi2_complete_kit__77966.jpg" style="width:100%"></a>
+            <a href="https://shop.openenergymonitor.com/energy-monitoring/"> <img src="<?php echo $path; ?>pages/img/emonpi3.png" style="width:100%"></a>
             <br>
             <h4 style="color:#000;margin-top:14px;">Expand your system with:</h4>
 
             <div style="text-align:left">
                 <p>
-                    <a href="https://shop.openenergymonitor.com/emontx5-emonpi2-6-ct-expansion-board/"><img src="<?php echo $path; ?>pages/img/ctsensor.png" style="float:left; width:50px; margin-right:20px" /></a>
-                    <b>Expand the emonPi2 to 12 CT channels:</b> Add a further 6 CT channels to the emonPi2 (Pi Zero 2W option only).
-                </p><br>
-                <p>
                     <a href="https://shop.openenergymonitor.com/emontx5-6-channel-energy-monitoring-transmitter-node/"><img src="<?php echo $path; ?>pages/img/emontx5-min.png" style="float:left; width:50px; margin-right:20px" /></a>
-                    <b>emonTx5:</b> A 6 channel electricity monitoring node. Transmits data via 433MHz radio to an emonPi or emonBase. Great for expanding an emonPi2 monitoring setup where more CT inputs are required either in the same location or in a different part of the building. Expand the emonTx5 to 12 CT channels with an optional expansion board.
+                    <b>emonTx6:</b> A 6 channel electricity monitoring node. Great for expanding an emonPi3 monitoring setup where more CT inputs are required either in the same location or in a different part of the building. Transmits data via 433.92MHz radio.
                 </p><br>
                 <p>
                     <a href="https://shop.openenergymonitor.com/emonth-temperature-humidity-node/"><img src="<?php echo $path; ?>pages/img/emonth-min.png" style="float:left; width:50px; margin-right:20px" /></a>
-                    <b>emonTH2:</b> A battery powered, wireless, room based temperature & humidity monitoring node. Transmits data via 433MHz radio to an emonBase or emonPi.
+                    <b>emonTH2:</b> A battery powered, wireless, room based temperature & humidity monitoring node. Transmits data via 433.92MHz radio to an emonPi3.
                 </p><br>
                 <p>
                     <a href="https://shop.openenergymonitor.com/heat-pump-monitoring/"><img src="<?php echo $path; ?>pages/img/heatmeter-min.png" style="float:left; width:50px; margin-right:20px" /></a>
@@ -66,7 +62,7 @@
             </div>
 
             <h4 style="color:#000;margin-top:14px;">Data logging and visualisation with EmonCMS:</h4>
-            <p>Local on the emonPi2/emonBase. Remote on <a href="https://emoncms.org" style="color:#2379d6">EmonCMS.org</a></p>
+            <p>Local on the emonPi3. Remote on <a href="https://emoncms.org" style="color:#2379d6">EmonCMS.org</a></p>
             <p>Pre built dashboards, advanced graphing tools, flexible input processing.</p>
             <a href="https://docs.openenergymonitor.org/emoncms/apps.html"> <img src="<?php echo $path; ?>pages/img/myheatpump.png" style="width:100%"></a>
 
